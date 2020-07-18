b8cd39ea33987385958483f1a02699336af034c1
Sergey <sergeyvolkovdev@gmail.com>
2020-07-18 14:25:00 +0300

    Updates

---
+++ precache-manifest.22f477f8a196bb8271850a0d378ce80b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f193e997792c8035165289dfd3eb35",
+    "url": "/mini-app/index.html"
+  },
+  {
+    "revision": "11f9ba026a11608cfeb3",
+    "url": "/mini-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ef27751a659ffc378dbf",
+    "url": "/mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "11f9ba026a11608cfeb3",
+    "url": "/mini-app/static/js/2.3086a1a6.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/mini-app/static/js/2.3086a1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef27751a659ffc378dbf",
+    "url": "/mini-app/static/js/main.77df88b2.chunk.js"
+  },
+  {
+    "revision": "1133b565d02bbdfe3f84",
+    "url": "/mini-app/static/js/runtime-main.af458bca.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/mini-app/static/media/persik.4e1ec840.png"
+  }
+]);
